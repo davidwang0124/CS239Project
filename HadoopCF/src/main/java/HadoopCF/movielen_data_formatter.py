16be33e9a9f37aa7b30b987a53b887d21ed09378
lyuanqi <liyuanqi@wifi-131-179-51-67.host.ucla.edu>
2017-03-08 18:55:30 -0800

    can now compile and run individual recommenderjob class

--- HadoopCF/src/main/java/HadoopCF/movielen_data_formatter.py
+++
@@ -1,9 +0,0 @@
-import re
-count=0
-with open('ratings.csv','r') as csv_file:
-   content = csv_file.readlines()
-   for line in content:
-        fixed = re.sub(",", "\t", line).rstrip()
-        splitted = fixed.split("\t")
-        if splitted[0]<>"userId":
-            print '%s' % fixed
