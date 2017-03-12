5047a78316caddc7290890d21bb7419efa052fec
wangym0124 <wangym0124@gmail.com>
2017-03-11 21:48:42 -0800

    add hdfs procedure

--- SparkCFR/cli.sh
+++ SparkCFR/cli.sh
@@ -1,3 +1,7 @@
 aws s3 cp s3://xgwang-spark-demo/ml-20m/movies.csv .
+aws s3 cp s3://xgwang-spark-demo/ml-20m/ratings.csv .
 aws s3 cp s3://xgwang-spark-demo/SparkCFR.jar .
+hadoop fs -put ratings.csv
 spark-submit SparkCFR.jar
+
+
