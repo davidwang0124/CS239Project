a87f4c9e383dcfc540ab8e536fa382838c437882
Xingan Wang <wangxgwxg@gmail.com>
2017-03-16 14:52:03 -0700

    cli update

--- SparkCFR/cli.sh
+++ SparkCFR/cli.sh
@@ -1,9 +1,15 @@
-aws s3 cp s3://xgwang-spark-demo/ml-10m/ratings.csv .
+aws s3 cp s3://xgwang-spark-demo/ml-20m/ratings.csv .
 aws s3 cp s3://xgwang-spark-demo/SparkCFR.jar .
 hadoop fs -put ratings.csv
 
 # m4.2xlarge
-spark-submit --master yarn  --driver-memory 20g  --driver-cores 6 --num-executors 4 --executor-cores 6 --executor-memory 20g --conf spark.driver.maxResultSize=20g SparkCFR-20m.jar 1
+spark-submit --master yarn  --driver-memory 20g  --driver-cores 6 --num-executors 2 --executor-cores 6 --executor-memory 20g --conf spark.driver.maxResultSize=20g SparkCFR.jar 1
 
-# 3 * m4.xlarge
+# m4.xlarge
 spark-submit --master yarn  --driver-memory 10g  --driver-cores 3 --num-executors 2 --executor-cores 3 --executor-memory 10g --conf spark.driver.maxResultSize=10g SparkCFR.jar 1
+
+# m4.4xlarge
+spark-submit --master yarn  --driver-memory 50g  --driver-cores 14 --num-executors 2 --executor-cores 14 --executor-memory 50g --conf spark.driver.maxResultSize=50g SparkCFR.jar 1
+
+# m4.16xlarge
+spark-submit --master yarn  --driver-memory 200g  --driver-cores 60 --num-executors 2 --executor-cores 60 --executor-memory 200g --conf spark.driver.maxResultSize=200g SparkCFR.jar 1
