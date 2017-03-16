2d6839eef728a11538d5e1f6cc1feba590a1e0e7
lyuanqi <liyuanqi@wifi-131-179-50-95.host.ucla.edu>
2017-03-16 15:49:39 -0700

    remove hadoop old output

--- CFR/movielen_data_formatter.py
+++ CFR/movielen_data_formatter.py
@@ -1,9 +1,15 @@
 import re
+import sys
+
+if len(sys.argv)<3:
+	print("usage: python movielen_data_formatter [input] [delimiter] > [output]\n")
+	exit(1)
+
 count=0
-with open('ratings.csv','r') as csv_file:
+with open(sys.argv[1],'r') as csv_file:
    content = csv_file.readlines()
    for line in content:
-        fixed = re.sub(",", "\t", line).rstrip()
+        fixed = re.sub(sys.argv[2], "\t", line).rstrip()
         splitted = fixed.split("\t")
         if splitted[0]<>"userId":
             print '%s' % fixed
