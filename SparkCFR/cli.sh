4eec9a607097ee8167a59ca599a5fd2d820230a3
Xingan Wang <wangxgwxg@gmail.com>
2017-03-17 11:22:42 -0700

    results from spark

--- SparkCFR/cli.sh
+++ SparkCFR/cli.sh
@@ -3,7 +3,7 @@ aws s3 cp s3://xgwang-spark-demo/SparkCFR.jar .
 hadoop fs -put ratings.csv
 
 # m4.2xlarge
-spark-submit --master yarn  --driver-memory 20g  --driver-cores 6 --num-executors 2 --executor-cores 6 --executor-memory 20g --conf spark.driver.maxResultSize=20g SparkCFR.jar 1
+spark-submit --master yarn  --driver-memory 20g  --driver-cores 7 --num-executors 2 --executor-cores 7 --executor-memory 20g --conf spark.driver.maxResultSize=20g SparkCFR.jar 1
 
 # m4.xlarge
 spark-submit --master yarn  --driver-memory 10g  --driver-cores 3 --num-executors 2 --executor-cores 3 --executor-memory 10g --conf spark.driver.maxResultSize=10g SparkCFR.jar 1
--- outputs/spark.md
+++ outputs/spark.md
@@ -3,5 +3,7 @@
 - 1 master, 4 core, m4.xlarge: 20min
 
 ## 20m
-- 1 master, 4 core, m4.xlarge: 48min
 - 1 master, 3 core, m4.xlarge: 41min
+- 1 master, 3 core, m4.2xlarge: 21min
+- 1 master, 3 core, m4.4xlarge: 12min
+- 1 master, 3 core, m4.10xlarge: 6.6min
